ebf1030af0ce39f7eb806d2057ebf9e128b80833
Diego3811 <diegohuci12@gmail.com>
2024-03-10 18:41:49 -0600

    We finished the landing page Co-authored-by: Ashrah <Ashrahx@users.noreply.github.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,9 @@ export default {
 				grayprimary: '#F6F6F6',
 				blurple: '#5865F2'
             },
+			boxShadow: {
+				'dc': '0 8px 5px rgba(0,0,0,.2)',
+            },
 		},
 	},
 	plugins: [animations],
